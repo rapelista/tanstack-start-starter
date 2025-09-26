59c4876c4d933b63547a3c4d97a5022e9d5f4ad9
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-26 11:03:06 +0700

    feat: initialize project structure with TanStack Router and Tailwind CSS
    - Added root route configuration in __root.tsx
    - Created index route for the home page in index.tsx
    - Generated route tree in routeTree.gen.ts
    - Implemented router setup in router.tsx
    - Included global styles with Tailwind CSS in globals.css
    - Configured TypeScript settings in tsconfig.json
    - Set up Vite configuration with necessary plugins for React and Tailwind CSS

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+  head: () => ({
+    meta: [
+      {
+        title: 'Home - Gvstang',
+      },
+    ],
+  }),
+});
+
+function RouteComponent() {
+  return <div>Hello "/"!</div>;
+}
