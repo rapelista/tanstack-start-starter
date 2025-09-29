c368ddf94a4b8af5f7edfdbe11c6adbe8e8efcf8
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-29 21:38:19 +0700

    fix(sign-up): redirect to dashboard after successful sign-up

--- src/components/_unauthed/sign-up/form.tsx
+++ src/components/_unauthed/sign-up/form.tsx
@@ -39,7 +39,7 @@ export function SignUpForm() {
           }),
         });
       } else {
-        router.navigate({ to: '/products' });
+        router.navigate({ to: '/dashboard' });
       }
     },
   });
